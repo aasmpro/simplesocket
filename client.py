a1949fc9ff81f7cab14ffc0dedb88d18b38fd57f
aasmpro <aa.smpro@gmail.com>
2018-06-24 20:17:23 +0430

    threading added.

--- client.py
+++ client.py
@@ -1,16 +1,14 @@
 from SimpleSocket import SimpleClientSocket
+from threading import Thread
+import atexit
 
 
-def main():
-    client = SimpleClientSocket.socket_from_sys()
-    print("connecting to {}:{} ...".format(client.host, client.port))
-    host, port = client.host, client.port
+def client_handle(client):
     while True:
-        client = SimpleClientSocket(sock=None, host=host, port=port)
-        client.connect()
         try:
             cmd = input("command >>> ")
-            if cmd == "exit": break
+            if cmd in ("[Exit]", "[E]"):
+                break
             client.send(cmd)
             msg = client.receive()
             print(msg)
@@ -18,7 +16,22 @@ def main():
         except:
             pass
 
+        client.send("[E]")
         client.close()
+
+
+def main():
+    client = SimpleClientSocket.socket_from_sys()
+    print("connecting to {}:{} ...".format(client.host, client.port))
+    client.connect()
+    receiver_thread = Thread(target=client_handle, args=(client,))
+    receiver_thread.start()
+    atexit.register(at_exit, client=client)
+
+
+def at_exit(client):
+    client.send("[E]")
+    client.close()
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,29 +1,43 @@
 from SimpleSocket import SimpleServerSocket
 import datetime
+from threading import Thread
+
+
+def main_func(client, server):
+    print("client {}:{} joined. # clients : {}".format(client.host, client.port, len(server.clients)))
+
+
+def clients_handel(client, server):
+    while True:
+        try:
+            cmd = client.receive()
+            if cmd not in ("[Exit]", "[E]"):
+                print("""client {}:{} >>> {}""".format(client.host, client.port, cmd))
+                msg = "Error : unknown command."
+                cmd = cmd.split()
+                if cmd[0] == "time":
+                    msg = 'time is {}'.format(datetime.datetime.now().time())
+                    client.send(msg)
+                elif cmd[0] == "[ALL]":
+                    server.broadcast(" ".join(cmd[1:]))
+                else:
+                    client.send(msg)
+            else:
+                client.close()
+                server.clients.remove(client)
+                print("""client {}:{} left. # clients : {}""".format(client.host, client.port, len(server.clients)))
+                break
+        except:
+            pass
 
 
 def main():
     server = SimpleServerSocket.socket_from_sys()
     print("\n\t{}\n\tserver started on {}:{}\n".format(datetime.datetime.now(), server.host, server.port))
     server.listen()
-
-    while True:
-        client = server.accept()
-        try:
-            cmd = client.receive()
-            if cmd:
-                print("""client {}:{} >>> {}""".format(client.host, client.port, cmd))
-                msg = "Error : unknown command."
-                cmd = cmd.split()
-
-                if cmd[0] == "time":
-                    msg = 'time is {}'.format(datetime.datetime.now().time())
-
-                client.send(msg)
-        except:
-            pass
-        
-        client.close()
+    accept_thread = Thread(server.accept_thread(clients_handel, main_func))
+    accept_thread.start()
+    accept_thread.join()
 
 
 if __name__ == '__main__':
