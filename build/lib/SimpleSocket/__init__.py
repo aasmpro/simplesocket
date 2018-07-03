457c9f5f9df322cae4a1eb388bf75397ba454492
aasmpro <aa.smpro@gmail.com>
2018-07-04 02:25:59 +0430

    fixing SimpleSocket/__init__.py

--- build/lib/SimpleSocket/__init__.py
+++ build/lib/SimpleSocket/__init__.py
@@ -1,7 +1,9 @@
+from .SimpleSocket import SimpleSocket, SimpleServerSocket, SimpleClientSocket
 __title__ = 'SimpleSocket'
 __version__ = '0.2.5'
 __author__ = 'Abolfazl Amiri'
 __author_email__ = 'aa.smpro@gmail.com'
-__license__ = 'GNU GENERAL PUBLIC LICENSE'
+__license__ = 'GNU General Public License v3 (GPLv3)'
+__all__ = ['SimpleSocket', 'SimpleServerSocket', 'SimpleClientSocket']
 
 VERSION = __version__
