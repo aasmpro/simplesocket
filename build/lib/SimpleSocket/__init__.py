b674d3dc7e48ef0cb2207bdc1a90ad2e8f901321
aasmpro <aa.smpro@gmail.com>
2018-07-04 03:23:49 +0430

    version 0.2.5.1 clean

--- build/lib/SimpleSocket/__init__.py
+++
@@ -1,9 +0,0 @@
-from .SimpleSocket import SimpleSocket, SimpleServerSocket, SimpleClientSocket
-__title__ = 'SimpleSocket'
-__version__ = '0.2.5.1'
-__author__ = 'Abolfazl Amiri'
-__author_email__ = 'aa.smpro@gmail.com'
-__license__ = 'GNU General Public License v3 (GPLv3)'
-__all__ = ['SimpleSocket', 'SimpleServerSocket', 'SimpleClientSocket']
-
-VERSION = __version__
